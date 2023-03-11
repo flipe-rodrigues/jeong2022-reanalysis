ad5e511f8459440878e8589a8752809badae71e2
flipe rodrigues <filipe.rodrigues@neuro.fchampalimaud.org>
2023-03-11 23:55:46 +0000

    experiment6 + cleanup

--- dependencies/tdlambda.m
+++ dependencies/tdlambda.m
@@ -63,14 +63,14 @@ function [state,value,rpe,weights,eligibility] = tdlambda(...
 %                 scaled_bases = unscaled_bases;
 %             end
             stimulus_temporal_features(idcs,:,ii) = ...
-                ...stimulus_temporal_features(idcs,:,ii) + ...
+                ...1/2 * stimulus_temporal_features(idcs,:,ii) + ...
                 temporal_bases(1:horizon,:);
         end
     end
 
     % concatenate representations across stimuli
     state = reshape(stimulus_temporal_features,n_states,n_stimuli*n_bases);
-
+    
     %% concatenate stimulus & temporal features
     stimulus_features = ...
         normalize01(stimulus_features,1) * max(temporal_bases,[],'all');
